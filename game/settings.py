457d201a41f99b9bfca78a5a17fa6d941f96ba89
Bruno Renié <buburno@gmail.com>
2011-06-18 12:37:10 +0200

    Enabling sentry

--- game/settings.py
+++ game/settings.py
@@ -6,3 +6,13 @@ except ImportError:
     pass
 
 DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
+
+INSTALLED_APPS += (
+    'sentry.client',
+)
+
+ADMINS = ()
+MANAGERS = ADMINS
+
+SENTRY_KEY = 'JXImOoyrVIaOEMxrWbUJ'
+SENTRY_REMOTE_URL = 'https://sentrybrute.ep.io/store/'
